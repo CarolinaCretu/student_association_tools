9d116f11b85388d763b04deb04a7c7f5fbb0aaa0
Carolina Cretu <150701412+CarolinaCretu@users.noreply.github.com>
2025-02-17 13:25:36 +0200

    Update email_automation.py

--- email automation/email_automation.py
+++ email automation/email_automation.py
@@ -6,26 +6,20 @@ import smtplib
 import pandas as pd
 
 
+email_sender = 'nextgenbiotech.fabit@gmail.com'
+email_password = input('password: ')
+
+# email_receivers = [""]
+
 path = input("path: ")
 df = pd.read_csv(path)
-
-email_sender = 'nextgenbiotech.fabit@gmail.com'
-# email_password = input('password: ')
-# email_receivers = [""]
 email_receivers = list(df.Email)
 
-subject = 'NextGen BioTech Newsletter - November'
+subject = 'Seminar - 4th March - Engineered Bacteriophages for Medical Applications'
 
 images = [
-    {"path": "Header Newsletter.png", "cid": "header"},
-    {"path": "Updated logo+name+slogan.png", "cid": "outro"},
-    {"path": "Recap.png", "cid": "recap"},
-    {"path": "Announce.png", "cid": "announcements"},
-    {"path": "Rec.png", "cid": "recommendations"},
-    {"path": "News.png", "cid": "news"},
-    {"path": "book.jpg", "cid": "book"},
-    {"path": "Founding team.jpg", "cid": "team"}
-
+    {"path": "poster.jpeg", "cid": "poster"},
+    {"path": "Updated logo+name+slogan.png", "cid": "logo"}
 ]
 
 
@@ -36,15 +30,16 @@ def send_email(receivers):
         my_email['From'] = email_sender
         my_email['To'] = person
         my_email['Subject'] = subject
-
+        
         for image in images:
             with open(image["path"], "rb") as img_file:
                 img = MIMEImage(img_file.read())
-                img.add_header("Content-ID",f"<{image['cid']}>")  # Match this ID in HTML
+                img.add_header("Content-ID",f"<{image['cid']}>")  
                 img.add_header("Content-Disposition", "inline", filename=image["path"])
                 my_email.attach(img)
 
-        html = open("email_body.html").read()
+        with open("email_body.html", encoding="utf-8") as f:
+            html = f.read()        
         my_email.attach(MIMEText(html, "html"))
 
         context = ssl.create_default_context()
@@ -52,11 +47,10 @@ def send_email(receivers):
             with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
                 smtp.login(email_sender, email_password)
                 smtp.sendmail(email_sender, person, my_email.as_string())
-                print("Email sent successfully!")
+                print(f"Email sent successfully!{person}")
         except Exception as e:
             print(f"Failed to send email: {e}")
 
 
-# send_email(email_receivers)
+send_email(email_receivers)
 
-print(email_receivers)
